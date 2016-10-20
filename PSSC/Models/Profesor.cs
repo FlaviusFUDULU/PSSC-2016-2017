4cb938a4da17a45ea847f1ed6a5de6a809d1cdd4
FlaviusFUDULU <code.flavi@gmail.com>
2016-10-20 16:42:42 +0300

    Just clases

--- PSSC/Models/Profesor.cs
+++ PSSC/Models/Profesor.cs
@@ -6,8 +6,7 @@ using System.Threading.Tasks;
 
 namespace Models
 {
-    public class Class1
+    class Profesor
     {
-        //remove or alter this class, then implement your models.  
     }
 }
